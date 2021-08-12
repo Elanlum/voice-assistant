1d5637e5513c7099e20f2fa085dd330e7e9d051e
Elanlum <piratone94@mail.ru>
2021-08-12 23:51:37 +0300

    Add: weather conditions based on geolocation (temperature)

---
+++ src/weather.py
@@ -0,0 +1,34 @@
+from pyowm.owm import OWM
+import configparser
+from pyowm.utils.config import get_default_config
+from location import get_location
+
+CRED_PROPERTIES = 'cred.properties'
+WEATHER_BLOCK = 'OpenWeather'
+
+
+def location():
+    info = get_location()
+    city = info['city']
+    country = info['country']
+    return city + ', ' + country
+
+
+def weather():
+    config = configparser.RawConfigParser()
+    config.read(CRED_PROPERTIES)
+
+    if config.has_option(WEATHER_BLOCK, 'weather.apikey'):
+        api_key = config.get(WEATHER_BLOCK, 'weather.apikey')
+
+    config = get_default_config()
+    config['language'] = 'ru'
+    owm = OWM(api_key, config)
+
+    mgr = owm.weather_manager()
+
+    weather_data = mgr.weather_at_place(location()).weather
+
+    temp_dict_celsius = weather_data.temperature('celsius')
+    temp_avg = temp_dict_celsius['temp']
+    return round(temp_avg)
