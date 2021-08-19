64378e63d72e391a8b0fa003bc9272370d39a959
Elanlum <piratone94@mail.ru>
2021-08-20 00:20:09 +0300

    Add: added translate function ru-en, en-ru

--- src/translator_service.py
+++ src/translator_service.py
@@ -1,7 +1,13 @@
 from googletrans import Translator
 
 
-def translate(text):
+def translate_en_ru(text):
     translator = Translator()
-    result = translator.translate(text, dest='ru')
+    result = translator.translate(text,src='en', dest='ru')
+    return result.text
+
+
+def translate_ru_en(text):
+    translator = Translator()
+    result = translator.translate(text, src='ru', dest='en')
     return result.text
