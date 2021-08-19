a13437d3e2dc7bd7bf93580ce769e3be45084f9e
Elanlum <piratone94@mail.ru>
2021-08-19 22:45:44 +0300

    Fix: weather_service reworked

--- src/weather_service.py
+++ src/weather_service.py
@@ -7,7 +7,7 @@ CRED_PROPERTIES = 'cred.properties'
 WEATHER_BLOCK = 'OpenWeather'
 
 
-def get_weather():
+def config_owm():
     config = configparser.RawConfigParser()
     config.read(CRED_PROPERTIES)
 
@@ -16,19 +16,22 @@ def get_weather():
 
     config = get_default_config()
     config['language'] = 'ru'
-    owm = OWM(api_key, config)
+    return OWM(api_key, config)
 
+
+def get_weather_info():
+    owm = config_owm()
     mgr = owm.weather_manager()
     location = location_info.get_region_city()
     return mgr.weather_at_place(location).weather
 
 
 def get_avg_temperature():
-    weather_data = get_weather()
-    temp_dict_celsius = weather_data.temperature('celsius')
+    weather_info = get_weather_info()
+    temp_dict_celsius = weather_info.temperature('celsius')
     temp_avg = temp_dict_celsius['temp']
     return round(temp_avg)
 
 
 def get_weather_status():
-    return get_weather().detailed_status
+    return get_weather_info().detailed_status
