80b41eeaba1bd2c1ef8fa622a772db155b775f7a
Elanlum <piratone94@mail.ru>
2021-09-05 19:12:35 +0300

    Fix: voice assistant time to listen is restricted so noise won't affect a reply

--- voice_assistant/bin/voice_recognizer.py
+++ voice_assistant/bin/voice_recognizer.py
@@ -6,12 +6,12 @@ language = config.get('Global', 'app.language')
 
 
 def recognize_voice():
-    print('Скажите что-нибудь >>>')
+    print('Говорите, пожалуйста >>>')
 
     voice_recognizer = sr.Recognizer()
     with sr.Microphone() as source:
         voice_recognizer.adjust_for_ambient_noise(source)
-        audio = voice_recognizer.listen(source)
+        audio = voice_recognizer.listen(source, None, 5)
 
     voice_text = voice_recognizer.recognize_google(audio, language=language)
 
