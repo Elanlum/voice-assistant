242816a59eeaa4767f297d26d6d89e241fd6ffa0
Elanlum <piratone94@mail.ru>
2021-09-09 00:12:36 +0300

    Add: two dictionaries: RU, EN for I18n

--- voice_assistant/bin/replicas_dictionary_en.py
+++ voice_assistant/bin/replicas_dictionary_en.py
@@ -1,11 +1,14 @@
-def dictionary():
+def dictionary_en():
     return {
-        'hello': 'Hello-hello!',
-        'bye': 'Bye!',
-        'how are you': 'I am fine, thanks',
-        'how is your day': 'It is quite good',
-        'music yandex': 'Playing Yandex',
-        'play yandex': 'Playing Yandex',
-        'turn on yandex': 'Playing Yandex',
-        'what is the weather': 'The temperature today'
+        'hello': ['hello', 'Hello-hello!'],
+        'bye': ['bye', 'Bye!'],
+        'how are you': ['how are you', 'I am fine, thanks'],
+        'how is your day': ['how is your day', 'It is quite good'],
+        'music yandex': ['music yandex', 'Playing Yandex'],
+        'play yandex': ['play yandex', 'Playing Yandex'],
+        'turn on yandex': ['turn on yandex', 'Playing Yandex'],
+        'what is the weather': ['what is the weather', 'The temperature today'],
+        'no replica': ['I don\'t get it', 'I don\'t get it'],
+        'what city': ['Which city', 'Which city'],
+        'city not found': ['City not found', 'City not found']
     }
