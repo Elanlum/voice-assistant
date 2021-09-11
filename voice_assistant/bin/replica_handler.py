0610229db2604728dbcdd08b50d3a452ef9bb68e
Elanlum <piratone94@mail.ru>
2021-09-12 00:30:23 +0300

    Update: added several TODOs

--- voice_assistant/bin/replica_handler.py
+++ voice_assistant/bin/replica_handler.py
@@ -17,9 +17,11 @@ replicas = dict(replicas)
 def handle_replica(user_text, tts_engine):
     user_text = user_text.lower()
 
+    # TODO: get rid of this
     response = ''
     replica = ''
 
+    # TODO: extract to a separate function returning tuple of (request, response)
     for r in replicas.keys():
         replica = r
         if select_request_replica(r) == user_text:
@@ -33,6 +35,7 @@ def handle_replica(user_text, tts_engine):
     if replica == const.PLAY_YANDEX or replica == const.MUSIC_YANDEX or replica == const.TURN_ON_YANDEX:
         reply_music(response, tts_engine)
     if replica == const.WHAT_WEATHER:
+        # TODO: extract logic on selecting city to a separate service, ask city until 'forget it' phrase
         reply(select_request_replica(const.SELECT_CITY), tts_engine)
         try:
             user_text_en = translate_ru_en(recognize_voice())
