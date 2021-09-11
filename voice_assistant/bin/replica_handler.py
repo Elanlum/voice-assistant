4bc10ea7302cecdf1ea0ac2207d842f0d24c2159
Elanlum <piratone94@mail.ru>
2021-09-11 23:35:13 +0300

    Fix: refactor of replica_handler: Exception handling deleted

--- voice_assistant/bin/replica_handler.py
+++ voice_assistant/bin/replica_handler.py
@@ -16,34 +16,30 @@ replicas = dict(replicas)
 def handle_replica(user_text, tts_engine):
     user_text = user_text.lower()
 
-    try:
-        response = ''
-        replica = ''
+    response = ''
+    replica = ''
 
-        for r in replicas.keys():
-            replica = r
-            if select_request_replica(r) == user_text:
-                response = select_response_replica(r)
-                break
-            else:
-                response = select_response_replica('no replica')
-
-        if replica == 'bye':
-            reply_bye(response, tts_engine)
-        if replica == 'play yandex' or replica == 'music yandex' or replica == 'turn on yandex':
-            reply_music(response, tts_engine)
-        if replica == 'what is the weather':
-            reply(select_request_replica('select city'), tts_engine)
-            try:
-                user_text_en = translate_ru_en(recognize_voice())
-                reply_weather(user_text_en, tts_engine)
-            except NotFoundError:
-                reply(select_request_replica('city not found'), tts_engine)
+    for r in replicas.keys():
+        replica = r
+        if select_request_replica(r) == user_text:
+            response = select_response_replica(r)
+            break
         else:
-            reply(response, tts_engine)
+            response = select_response_replica('no replica')
 
-    except KeyError:
-        reply('Не понимаю Вас', tts_engine)
+    if replica == 'bye':
+        reply_bye(response, tts_engine)
+    if replica == 'play yandex' or replica == 'music yandex' or replica == 'turn on yandex':
+        reply_music(response, tts_engine)
+    if replica == 'what is the weather':
+        reply(select_request_replica('select city'), tts_engine)
+        try:
+            user_text_en = translate_ru_en(recognize_voice())
+            reply_weather(user_text_en, tts_engine)
+        except NotFoundError:
+            reply(select_request_replica('city not found'), tts_engine)
+    else:
+        reply(response, tts_engine)
 
 
 def select_request_replica(replica):
