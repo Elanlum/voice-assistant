306550137bb31da9333419e6fe5e37888d95e995
Elanlum <piratone94@mail.ru>
2021-09-15 12:50:02 +0300

    Add: an insult for Siri as a command

--- voice_assistant/bin/replicas_dictionary_en.py
+++ voice_assistant/bin/replicas_dictionary_en.py
@@ -8,6 +8,7 @@ def dictionary_en():
         BYE: ['bye', 'Bye!'],
         'how are you': ['how are you', 'I am fine, thanks'],
         'how is your day': ['how is your day', 'It is quite good'],
+        'who\'s Siri': ['who\'s siri', 'I don\'t know that bitch but I don\'t like her'],
         MUSIC_YANDEX: ['music yandex', 'Playing Yandex'],
         PLAY_YANDEX: ['play yandex', 'Playing Yandex'],
         TURN_ON_YANDEX: ['turn on yandex', 'Playing Yandex'],
