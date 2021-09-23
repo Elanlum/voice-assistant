55ad9018dc41720e4c7248e4f49844ec55bce53c
Elanlum <piratone94@mail.ru>
2021-09-23 23:58:55 +0300

    Add: file reader service implementation

--- voice_assistant/bin/service/file_service.py
+++ voice_assistant/bin/service/file_service.py
@@ -1,18 +1,26 @@
 import pathlib
+import os
 
 directory = pathlib.Path.cwd()
 
 
-def open_file(os, filename):
+def open_file(system, file_name_type):
+    (filename, filetype) = select_file_type(file_name_type)
 
-    if os == 'Windows':
-        os.system(f'explorer {directory}\\files\\{filename}')
-    elif os == 'Darwin':
+    if system == 'Windows':
+        os.system(f'explorer {directory}\\files\\{filename}.pdf')
+    elif system == 'Darwin':
         os.system(f'open {directory}/files/{filename}')
     else:
-        os.system(f'xdg-open {directory}/files/{filename}')
+        system.system(f'xdg-open {directory}/files/{filename}')
 
 
-def select_file_type():
-    # TODO: implement
-    return
+def select_file_type(file_name_type):
+    supported_filetypes = ['pdf', 'audio', 'word', 'excel']
+
+    for filetype in supported_filetypes:
+        if filetype in file_name_type:
+            filename = file_name_type.replace(filetype, '').strip()
+            return filename, filetype
+
+    raise NotImplementedError('You tried to open unsupported type of file or type is not recognized')
